ce17d8583c579ab7764ee343d3087b4bc9609425
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-02-25 22:56:14 -0800

    Create header for app.

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,16 +1,17 @@
 // defining a component
 import React from "react";
+import { Navbar } from 'react-bootstrap';
 
 function Header() {
-    return (
-        // header component.
-        // employee list component
-        // employlee list will map through all employee
-        <div>
-            <h2>employee directory</h2>
-            <p>Text goes here</p>
-        </div>
-    )
+  return (
+
+    // header component.
+    // employee list component
+    // employlee list will map through all employee
+    <Navbar expand="lg" style={{ backgroundColor: "lightpink" }}>
+      <Navbar.Brand href="#home" style={{ marginLeft: "50%", marginTop: "2rem", height: "5rem" }}><h2>Employee Directory</h2> <p>Sort employees by headers below.</p></Navbar.Brand>
+    </Navbar>
+  )
 }
 
 export default Header
